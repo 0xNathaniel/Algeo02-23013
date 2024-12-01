a817bfd96ea3573b372759f34f99b8c8b9e2faec
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-12-02 03:48:38 +0700

    feat: add driver

--- src/Backend/Album Picture Finder/data_centering.py
+++ src/Backend/Album Picture Finder/data_centering.py
@@ -8,6 +8,22 @@ def count_average(grayscale_values):
 # Standardize an image's grayscale values
 def standardize(grayscale_values):
     # Standardize or center each pixels
-    standardized_values -= count_average(grayscale_values)
+    standardized_values = grayscale_values - count_average(grayscale_values)
     # Return standarized grayscale values
     return standardized_values
+
+# Driver
+def main():
+    grayscale_values = np.array([120, 125, 130])
+
+    print("Original Grayscale Values:")
+    print(grayscale_values)
+
+    # Standardize the grayscale values
+    standardized_values = standardize(grayscale_values)
+
+    print("\nStandardized Grayscale Values:")
+    print(standardized_values)
+
+if __name__ == "__main__":
+    main()
