bb9a953e9ae83087e8421894acb8324860533e52
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-12-06 04:40:15 +0700

    feat: add get_principal_component

--- src/Backend/Album Picture Finder/pca.py
+++ src/Backend/Album Picture Finder/pca.py
@@ -2,8 +2,10 @@ import numpy as np
 
 def perform_pca(images, n_components):
     U, S, Vt = np.linalg.svd(images, full_matrices=False)
-    principal_components = Vt[:n_components]
-    return principal_components, U, S
+    return U, S, Vt
+
+def get_principal_components(eigenvector, n_components):
+    return eigenvector[:n_components]
 
 def project_images(images, principal_components):
     return np.dot(images, principal_components.T)
--- src/Backend/Album Picture Finder/retrieval_and_output.py
+++ src/Backend/Album Picture Finder/retrieval_and_output.py
@@ -3,21 +3,22 @@ import numpy as np
 from PIL import Image
 from image_processing_and_loading import load_and_process_images
 from data_centering import standardize_data
-from pca import perform_pca, project_images
+from pca import perform_pca, get_principal_components, project_images
 from similarity import calculate_euclidean_distance
 
 # Parameters
 image_directory = "src/Backend/Album Picture Finder/Album Pictures"  # Change to your image directory
 query_image_name = "0.jpg"  # Change to your query image name
 n_components = 8  # Number of principal components
-n_images = 100  # Number of top similar images to return
+n_images = 5  # Number of top similar images to return
 resize_dim = 512  # Resize dimension (images will be resized to resize_dim x resize_dim)
 
 def main():
     # Load and process database images
     images, image_files = load_and_process_images(image_directory, resize_dim)
     standardized_images, mean = standardize_data(images)
-    principal_components, _, _ = perform_pca(standardized_images, n_components)
+    _, _, eigenvector = perform_pca(standardized_images, n_components)
+    principal_components = get_principal_components(eigenvector, n_components)
     image_projections = project_images(standardized_images, principal_components)
     
     # Load and process the query image
@@ -30,7 +31,6 @@ def main():
     # Calculate distances or similiarity
     distances = calculate_euclidean_distance(query_projection, image_projections)
     
-    # Sort most similar images
     sorted_indices = np.argsort(distances)
     top_n_indices = sorted_indices[:n_images]
     
