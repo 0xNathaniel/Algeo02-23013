606da7c823c5ed51ca2e00db3eb31f9e31ad0ede
Nathaniel Jonathan Rusli <19623113@std.stei.itb.ac.id>
2024-12-06 04:27:56 +0700

    refactor: use different approach

--- src/Backend/Album Picture Finder/image_processing_and_loading.py
+++ src/Backend/Album Picture Finder/image_processing_and_loading.py
@@ -1,24 +1,17 @@
+import os
 import numpy as np
 from PIL import Image
 
-# Loads and process image into a 1D list of grayscale values
-def load_and_process(image_path):
-    # Load image and convert the pixels into the RGB format
-    img = Image.open(image_path).convert("RGB")
-    # Convert image to NumPy array
-    img_array = np.array(img)
-    # Calculate grayscale values using I(x,y) = 0.2989.R(x,y) + 0.5870.G(x,y) + 0.1140.B(x,y)
-    grayscale_values = (
-        0.2989 * img_array[:, :, 0] +  # Red 
-        0.5870 * img_array[:, :, 1] +  # Green 
-        0.1140 * img_array[:, :, 2]    # Blue 
-    )
-    # Return flattened 2D grayscale array (1D array)
-    return grayscale_values.flatten()
-
-# Driver
-if __name__ == "__main__":
-    directory = ""
-    file_name = input("Test image file name: ")
-    grayscale_values = load_and_process(directory + file_name)
-    print(grayscale_values)
+def load_and_process_images(image_directory, resize_dim):
+    image_files = [f for f in os.listdir(image_directory) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    images = []
+    
+    for image_file in image_files:
+        image_path = os.path.join(image_directory, image_file)
+        image = Image.open(image_path)
+        grayscale_image = image.convert('L')
+        resized_image = grayscale_image.resize((resize_dim, resize_dim))
+        image_array = np.array(resized_image).flatten()
+        images.append(image_array)
+    
+    return np.array(images), image_files
