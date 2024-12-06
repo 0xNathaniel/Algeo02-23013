378ea4ec403d33eefcea6828764cf0383f93d2b3
0xNathaniel <13523113@std.stei.itb.ac.id>
2024-12-06 15:17:25 +0700

    feat: add api and change code and file structure

--- src/Backend/Album Picture Finder/retrieval_and_output.py
+++ src/Backend/Album Picture Finder/retrieval_and_output.py
@@ -6,38 +6,30 @@ from data_centering import standardize_data
 from pca import perform_pca, get_principal_components, project_images
 from similarity import calculate_euclidean_distance
 
-# Parameters
-image_directory = "src/Backend/Album Picture Finder/Album Pictures"  # Change to your image directory
-query_image_name = "0.jpg"  # Change to your query image name
-n_components = 8  # Number of principal components
-n_images = 5  # Number of top similar images to return
-resize_dim = 512  # Resize dimension (images will be resized to resize_dim x resize_dim)
-
-def main():
-    # Load and process database images
+def preprocess_database_images(image_directory, resize_dim, n_components):
     images, image_files = load_and_process_images(image_directory, resize_dim)
     standardized_images, mean = standardize_data(images)
-    _, _, eigenvector = perform_pca(standardized_images, n_components)
+    _, _, eigenvector = perform_pca(standardized_images, n_components)  
     principal_components = get_principal_components(eigenvector, n_components)
     image_projections = project_images(standardized_images, principal_components)
-    
-    # Load and process the query image
-    query_image = Image.open(os.path.join(image_directory, query_image_name)).convert('L')
-    query_image_resized = query_image.resize((resize_dim, resize_dim))
-    query_image_array = np.array(query_image_resized).flatten()
-    query_image_standardized = query_image_array - mean
-    query_projection = np.dot(query_image_standardized, principal_components.T)
-    
-    # Calculate distances or similiarity
-    distances = calculate_euclidean_distance(query_projection, image_projections)
-    
-    sorted_indices = np.argsort(distances)
-    top_n_indices = sorted_indices[:n_images]
-    
-    # Display the results
-    print(f"Top {n_images} most similar images to {query_image_name}:")
-    for rank, index in enumerate(top_n_indices, start=1):
-        print(f"{rank}. {image_files[index]} with distance {distances[index]}")
+    return image_files, mean, principal_components, image_projections
 
-if __name__ == "__main__":
-    main()
+def preprocess_query_image(mean, query_image, resize_dim, principal_components):
+    # Preprocess the query image using the calculated
+    grayscale_image = query_image.convert('L')
+    resized_image = grayscale_image.resize((resize_dim, resize_dim))
+    query_image_array = np.array(resized_image).flatten()
+        
+    # Standardize the query image
+    query_image_standardized = query_image_array - mean
+        
+    # Project the query image onto the principal components
+    query_projection = np.dot(query_image_standardized, principal_components.T)
+    return query_projection
+
+def output_similarity(query_projection, image_projections, top_n_images):
+    distances = calculate_euclidean_distance(query_projection, image_projections)
+    sorted_indices = np.argsort(distances)
+    top_n_indices = sorted_indices[:top_n_images]
+
+    return distances, sorted_indices, top_n_indices
